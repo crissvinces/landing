5128e7af519317147efe6f5596e04b3da49298a6
crissvinces <cvinces@espol.edu.ec>
2024-06-03 14:55:14 -0500

    Avance

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let loaded = ( eventLoaded ) => {
+  
+    window.alert("landing page loaded");
+    console.log( eventLoaded );
+    debugger;
+
+    let myform = document.getElementById('<form>');
+  debugger;
+  }
+  
+  window.addEventListener("DOMContentLoaded", loaded);
+
