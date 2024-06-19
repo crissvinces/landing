5ab28ee28a8b7c573d9576e01d514b88e0ca4566
crissvinces <cvinces@espol.edu.ec>
2024-06-18 23:31:14 -0500

    Avance

--- js/main.js
+++ js/main.js
@@ -1,12 +1,42 @@
 let loaded = ( eventLoaded ) => {
-  
-    window.alert("landing page loaded");
-    console.log( eventLoaded );
-    debugger;
 
-    let myform = document.getElementById('<form>');
-  debugger;
-  }
+    let myform = document.getElementById('form');
   
-  window.addEventListener("DOMContentLoaded", loaded);
-
+    myform.addEventListener('submit', ( eventSubmit ) => { 
+      eventSubmit.preventDefault();
+      let nombre_valor= nombre.value;
+      let correo_valor=correo.value;
+      let interes_valor=interes.value;
+      if( nombre_valor.length == 0 ) {
+      
+        nombre_valor.focus()
+  
+        alert('Ingrese un texto válido')
+  
+        return;
+      }
+      if( correo_valor.length == 0 ) {
+      
+        correo_valor.focus()
+  
+        alert('Ingrese un texto válido')
+  
+        return;
+      }
+      if( interes_valor.length == 0 ) {
+      
+        interes_valor.focus()
+  
+        alert('Ingrese un texto válido')
+  
+        return;
+      }
+  
+  
+  
+      debugger;
+  
+    })
+  
+  
+  window.addEventListener("DOMContentLoaded", loaded);}
